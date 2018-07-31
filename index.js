872ab3123025e6c7b0a2cee95e9cf76840a5bc0c
riyadh hossain <riyadh_h@ymail.com>
2018-07-31 21:24:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,9 +69,8 @@ function removeFromCart(item) {
       //var ret =
       return cart.splice(i,1)
     }
-
     }
-      return "That item is not in your cart."
+    return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
