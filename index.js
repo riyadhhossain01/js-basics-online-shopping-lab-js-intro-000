81d8bcd864093fd581e40c3bc1b81c16001be8a4
riyadh hossain <riyadh_h@ymail.com>
2018-07-31 21:13:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,17 +62,17 @@ function total() {
 
 }
 
-// function removeFromCart(item) {
-//   // write your code here
-//   // for(let i = 0; i<cart.length; i++){
-//   //   if(item == cart[i].itemName){
-//   //     var ret = cart.splice(i,1)
-//   //     return ret
-//   //   }else{
-//   //     return "That item is not in your cart."
-//   //     }
-//   //   }
-// }
+function removeFromCart(item) {
+  //write your code here
+  for(let i = 0; i<cart.length; i++){
+    if(item == cart[i].itemName){
+      var ret = cart.splice(i,1)
+      return ret
+    }else{
+      return "That item is not in your cart."
+      }
+    }
+}
 
 function placeOrder(cardNumber) {
 // write your code here
