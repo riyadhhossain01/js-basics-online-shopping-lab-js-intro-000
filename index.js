b8f4ab1a48d532b5bb7905c333cffa2c8f031f02
riyadh hossain <riyadh_h@ymail.com>
2018-07-31 21:16:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,8 +66,8 @@ function removeFromCart(item) {
   //write your code here
   for(var i = 0; i < cart.length; i++){
     if(item == cart[i].itemName){
-      var ret = cart.splice(i,1)
-      return ret
+      //var ret =
+      return cart.splice(i,1)
     }else{
       return "That item is not in your cart."
       }
