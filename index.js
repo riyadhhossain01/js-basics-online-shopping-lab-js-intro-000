0e4c1e67f3f329624dd7c0b67dd039caa2da0824
riyadh hossain <riyadh_h@ymail.com>
2018-07-31 21:17:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function total() {
 function removeFromCart(item) {
   //write your code here
   for(var i = 0; i < cart.length; i++){
-    if(item == cart[i].itemName){
+    if(item === cart[i].itemName){
       //var ret =
       return cart.splice(i,1)
     }else{
